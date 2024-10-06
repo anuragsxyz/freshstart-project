78dce3a37ccc1e35cbc8ce9f8beebecd1dc612f1
anuragsxyz <anurag.saraswat95@yahoo.com>
2024-10-06 17:46:06 +0530

    json data

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const randomData = require("./randomdata.json");
+
+app.use(express.static("public")); // static directory
+
+const path = require("path");
+
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "ejs");
+
+app.get("/random", (req, res) => {
+  let rand = Math.floor(Math.random() * 10);
+  res.render("random", { rand });
+});
 
 app.get("/", (req, res) => {
-  res.send("Hey Server!!");
+  const randomDatafile = randomData;
+  const cats = ["mike", "mia", "lovely", "modu"];
+  let rand = Math.floor(Math.random() * 10);
+  res.render("index", { cats, randomDatafile });
 });
 
-app.get("/admin", (req, res) => {
-  res.render("admin");
+app.listen(port, () => {
+  console.log("running");
 });
-
-app.listen(port, () => "hey, server running");
